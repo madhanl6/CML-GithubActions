0d181d911c621f0db6881330c26d2bbca63f3c65
Madhi Madhan Libonce <87375403+madhanl6@users.noreply.github.com>
2023-11-17 00:10:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -98,6 +98,6 @@ mae_1 = np.round(float(mae(y_test, y_preds.squeeze()).numpy()), 2)
 mse_1 = np.round(float(mse(y_test, y_preds.squeeze()).numpy()), 2)
 print(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
 
-# Write metrics to file
+# Writing the metrics to file
 with open('metrics.txt', 'w') as outfile:
     outfile.write(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
